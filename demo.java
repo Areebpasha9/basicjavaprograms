c263f1d89487d5bae22005a94d745001b0dca658
Areebpasha9 <areebpasha9548@gmail.com>
2024-08-27 23:25:17 +0530

    updated

--- demo.java
+++ demo.java
@@ -1,5 +1,26 @@
+/**
+ * demo
+ */
 public class demo {
-    public static void main(String[] args) {
-        System.out.println("mohammad");
+
+  public static void main(String[] args) {
+    int ar[] = { 1, 2, 1, 3, 1, 4, 1, 2, 5 };
+    for (int i = 0; i < ar.length; i++) {
+      boolean flag = false;
+      if (ar[i] == -1) {
+        continue;
+      }
+      for (int j = i + 1; j < ar.length; j++) {
+        if (ar[i] == ar[j]) {
+          // System.out.println(ar[i]);
+          flag = true;
+          ar[j] = -1;
+
+        }
+      }
+      if (flag) {
+        System.out.println(ar[i]);
+      }
     }
+  }
 }
